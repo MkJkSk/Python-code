3d659f81942e4e2b59e01a235813606d1e6877af
Muzammil Ahmed Khan <muzammilkhan199936@gmail.com>
2022-08-30 23:09:20 -0700

    Practice 2

--- set.py
+++ set.py
@@ -1,4 +1,21 @@
-# set in  not consider a dupilcate element
-s ={12,33,44,54,6,7,8,9}
-s.remove(54)
-print(s)
+# # set in  not consider a dupilcate element
+# s ={12,33,44,54,6,7,8,9}
+# s.remove(54)
+# print(s)
+
+
+# from collections import deque
+
+# sw = ["Form Beginners",20]
+#     ["Form Beginners",10] 
+#     ["Form Beginners",12]
+#     ["Form Beginners",11]
+
+
+
+
+
+
+
+
+
---
+++ swap.py
@@ -0,0 +1,7 @@
+x = 10
+y = 20
+
+x,y = y,x
+print("\tSwapping Number is given Below\t")
+print(f"x={x}")
+print(f"y={y}")
