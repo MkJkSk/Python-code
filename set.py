523c29426686d6c39ca8f03a427eb2d3fc16d741
Muzammil Ahmed Khan <muzammilkhan199936@gmail.com>
2022-08-30 22:59:19 -0700

    set

---
+++ set.py
@@ -0,0 +1,4 @@
+# set in  not consider a dupilcate element
+s ={12,33,44,54,6,7,8,9}
+s.remove(54)
+print(s)
--- unpacking.py
+++ unpacking.py
@@ -1,4 +1,4 @@
-numbers = [2,3,4,5,6,7,8,9]
+numbers = [20,3,4,5,6,7,8,90]
 
 first, second, *other = numbers
 print(f"{first} {second} {other}")
