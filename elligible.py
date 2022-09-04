8a4272defbe2b33dab600d9cf166bffce1dad625
Muzammil Ahmed Khan <muzammilkhan199936@gmail.com>
2022-09-04 14:42:28 -0700

    04-Sept-2022

--- elligible.py
+++ elligible.py
@@ -1,6 +1,10 @@
-age = 70
+age= int(input("Enter Your age:\n"))
 
-if 18<=age<=65:
-    print(" You are Eligible for job")
+if 18<=age<=58:
+    print("""\tYou are Eligible for job.Welcome to our company Your Package\t
+    \tis 8LPA and additional bonus 1 lakh\t""")
 else:
-    print(" You are Not Eligible for job")    
+    print("""\tYou are Not Eligible for job\t
+    \tBecause Your age is on retirement Thank You and take\t
+    \trest at home.\t
+    """)    
